d6fa077dd810f9483fe30521db9f00eb62434104
shancy1998 <shyams3946@gmail.com>
2026-09-16 14:25:47 +0530

    fix fire alarm and theft alarm imeges

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'YOUR_SUPABASE_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://bgcsmwrqkujkgmujobxn.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_KtJzFbC82YvllhlQAEgrvA_xuTDJmHK';
 const STORAGE_BUCKET = 'website-images';
 
 const loginPanel = document.getElementById('login-panel');
